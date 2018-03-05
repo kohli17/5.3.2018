9db447276c0e1451770af94e934928ce6be7d1ea
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-03-05 16:00:45 +0530

    Create TEMPERATURE.C
    print the temperature

---
+++ TEMPERATURE.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+    int c;
+    float k;
+   printf("enter the celsius");
+   scanf("%d",&c);
+   k=c+273.15;
+   printf(" kelvin %f",k);
+    return 0;
+    getch();
+}
+
+
