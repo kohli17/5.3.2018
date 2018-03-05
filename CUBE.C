80952c0decfe02b43e21d9d0c14e518b9e2fa01e
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-03-05 15:53:10 +0530

    Create CUBE.C
    print the volume

---
+++ CUBE.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#includwe<conio.h>
+int main()
+{
+	int l,b,h,tsa,volume;
+	scanf("%d %d %d",&l,&b,&h);
+	volume=l*b*h;
+	tsa=2*((l*b)+(b*h)+(h*l));
+	printf("volume=%d\n",volume);
+	printf("TSA=%d",tsa);
+	return 0;
+  getch();
+}
